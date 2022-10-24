caa9658242c600e4a7b8acec4a48b7f6f7d454c9
Nickeem <54283992+Nickeem@users.noreply.github.com>
2022-10-24 17:28:05 -0400

    create download shell

---
+++ download.sh
@@ -0,0 +1,1 @@
+docker run --restart unless-stopped --name=blueocean --group-add $(stat -c '%g' /var/run/docker.sock) -p 8888:8080 -d -v jenkins-data:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock jenkins/jenkins:latest
